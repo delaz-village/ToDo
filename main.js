1029cd4981519d6dccf9ba6d13d28add3b50bc9a
delaz-village <quasar1031@yahoo.co.jp>
2023-02-26 15:52:43 +0900

    コミット

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const addBtn = document.getElementsByClassName("add-btn");
 const todo = document.getElementById("todo");
 
-addBtn[0].addEventListener("click",add);
+addBtn[0].addEventListener("click", add);
 
-function add(){
+function add() {
 	let task = document.getElementById("add-area").value;
-	if(task){
+	if (task.trim()) {
 		let elem = document.createElement("li");
 		elem.innerHTML = `${task}<button onclick = "del(this)">完了</button>`;
 		todo.appendChild(elem);
 		document.getElementById("add-area").value = "";
-	}else{
+	} else {
 		alert("空欄です！");
 	}
 }
-function del(delTask){
+function del(delTask) {
 	delTask.parentNode.remove();
 }
 
